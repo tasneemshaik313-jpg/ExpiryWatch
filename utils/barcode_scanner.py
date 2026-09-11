539463b5e0e6477847fa88a854e73eb24323eee5
Tasneem.Shaik <tasneemshaik313@gmail.com>
2026-09-11 19:48:09 +0530

    Update barcode_scanner.py

--- utils/barcode_scanner.py
+++ utils/barcode_scanner.py
@@ -1,14 +1,23 @@
+try:
+    from pyzbar.pyzbar import decode
+except Exception:
+    decode = None
 
-from pyzbar.pyzbar import decode
 from PIL import Image
 
 
 def scan_barcode(image_path):
+    if decode is None:
+        return None
+
     try:
         image = Image.open(image_path)
         results = decode(image)
+
         if results:
             return results[0].data.decode("utf-8", errors="ignore")
+
     except Exception as e:
         print("Barcode scan error:", e)
+
     return None
